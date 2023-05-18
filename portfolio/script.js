edc6ceb8623d5580700289f4dcfc42c576602b33
Ricardo Martins <riicardo.marttyns@gmail.com>
2023-05-18 15:54:16 -0300

    :up: Portfólio #3

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,15 +1,29 @@
-
-
-function ativaLetra(elemento){
-  const arrTexto = elemento.innerHTML.split('');
-  elemento.innerHTML = '';
-  arrTexto.forEach((letra, i) => {
-    setTimeout(() => {
-      elemento.innerHTML += letra;
-    }, 75 * i);
-  })
+//HERO
+function escrevendoLetra(){
+  
+  function ativaLetra(elemento){
+    const arrTexto = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    arrTexto.forEach((letra, i) => {
+      setTimeout(() => {
+        elemento.innerHTML += letra;
+      }, 75 * i);
+    })
+  }
+  
+  const titulo = document.querySelector('.digitando');
+  
+  ativaLetra(titulo);
 }
 
-const titulo = document.querySelector('.digitando');
+escrevendoLetra();
 
-ativaLetra(titulo);
+//MENU
+const ativaMenu = document.querySelector('.fa-bars');
+const navMenu = document.querySelector('header .navegacao-primaria');
+
+ativaMenu.addEventListener('click', () => {
+  ativaMenu.classList.toggle('fa-x');
+  navMenu.classList.toggle('ativado');
+})
+
